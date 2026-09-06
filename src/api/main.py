95ca836ffacbb57530b0794375f307a7999c76c0
hhlbby <artempatskov2007@gmail.com>
2026-09-06 22:55:19 +0300

    feat: add predict endpoint with db logging

--- src/api/main.py
+++ src/api/main.py
@@ -1,5 +1,12 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, Depends, status
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+from src.api.schemas import TransactionInput, PredictionOutput
+from src.db.database import engine, Base, get_db
+from src.db.crud import create_transaction_log
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="FinTech Fraud Detection Microservice",
@@ -21,7 +28,6 @@ class HealthCheckResponse(BaseModel):
     summary="Проверка работоспособности сервиса",
 )
 async def healthcheck() -> HealthCheckResponse:
-    """Эндпоинт для проверки здоровья сервиса."""
     return HealthCheckResponse(status="OK", version=app.version)
 
 
@@ -30,3 +36,30 @@ async def root():
     return {
         "message": "Fraud Detection API is running. Go to /docs for Swagger UI."
     }
+
+
+@app.post(
+    "/predict",
+    response_model=PredictionOutput,
+    status_code=status.HTTP_200_OK,
+    tags=["ML Inference"],
+    summary="Скоринг транзакции на мошенничество",
+)
+async def predict(
+    transaction: TransactionInput, 
+    db: Session = Depends(get_db)
+) -> PredictionOutput:
+
+    # Временно
+    dummy_probability = 0.85 if transaction.Amount > 5000 else 0.02
+    is_fraud = dummy_probability > 0.5
+
+    prediction = PredictionOutput(
+        is_fraud=is_fraud,
+        fraud_probability=dummy_probability,
+        model_version="0.1.0-mock",
+    )
+
+    create_transaction_log(db=db, transaction=transaction, prediction=prediction)
+
+    return prediction
